882df14f4a4a4765332df7077cc2fb4773b63f9d
Mohammedily <m9741305@gmail.com>
2022-03-30 18:30:28 +0530

    dsdsfsfsf

--- models/product.js
+++ models/product.js
@@ -3,6 +3,10 @@ const mongoose = require("mongoose");
 
 
 const productSchema = new mongoose.Schema({
+    id:{
+        type:Number,
+        required: true
+    },
     title: {
         type:String,
         required: true
